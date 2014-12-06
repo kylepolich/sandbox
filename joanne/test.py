1a50b63b31a67aab38e44ee2e6e6f818395da2ae
Kyle Polich <kylepolich@gmail.com>
2014-12-06 15:10:49 -0800

    adding some test files

---
+++ joanne/test.py
@@ -0,0 +1,17 @@
+import os
+
+abspath = os.getcwd()
+print 'My absolute path is ' + abspath
+
+# A list of scientists
+scientists = ['Bohr', 'Curie', 'Archimedes', 'Kepler']
+
+# A dictionary / map of birth years
+byears = {}
+byears['Bohr']       = 1885
+byears['Curie']      = 1867
+byears['Archimedes'] = 252
+byears['Kepler']     = 1571
+
+# Can you add your favorite scientist to the list and put their birthyear in the dictionary?
+
